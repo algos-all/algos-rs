cacd5050cacbb5dc9cceeac879821ff0128a4bc0
Aleksandr Lisianoi <alisianoi@protonmail.com>
2021-10-12 10:20:42 +0200

    Add IntoIter that consumes the collection (returns by value)

--- src/linked_list_02.rs
+++ src/linked_list_02.rs
@@ -55,6 +55,43 @@ impl<T> List<T> {
     }
 }
 
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn iter<'a>(&'a self) -> Iter<'a, T> {
+        Iter { next: self.head.as_deref() }
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref().map(|node| &*node);
+            &node.elem
+        })
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::Link;
@@ -121,5 +158,20 @@ mod tests {
         });
 
         assert_eq!(xs.peek(), Some(&99));
+    }
+
+    #[test]
+    fn list_iter_00() {
+        let mut xs = List::new();
+
+        xs.push(1);
+        xs.push(2);
+        xs.push(3);
+
+        let mut it = xs.into_iter();
+        assert_eq!(it.next(), Some(3));
+        assert_eq!(it.next(), Some(2));
+        assert_eq!(it.next(), Some(1));
+        assert_eq!(it.next(), None);
     }
 }
