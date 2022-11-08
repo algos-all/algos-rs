7a7ae2e78010656655128fb13f7226785f3377b2
Aleksandr Lisianoi <alisianoi@protonmail.com>
2022-11-08 21:33:56 +0100

    Add basic sort experiments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+#[derive(Debug, PartialEq, PartialOrd, Eq, Ord)]
+struct Rectangle {
+    x: i32,
+    y: i32,
+    side_x: i32,
+    side_y: i32,
+}
+
 pub fn main() {
-    println!("Hello, world!");
+    let mut xs = vec![
+        Rectangle {
+            x: 0,
+            y: 0,
+            side_x: 10,
+            side_y: 20,
+        },
+        Rectangle {
+            x: 3,
+            y: 9,
+            side_x: 4,
+            side_y: 8,
+        },
+    ];
+
+    xs.sort_by(|lft, rgt| lft.side_x.cmp(&rgt.side_x));
+
+    println!("{:#?}", xs);
 }
